ea3d78a7cc90cfece8a32fa4ca831ad80d228cfa
Lynx <chase.knox.f@gmail.com>
2026-07-20 19:26:57 -0300

    style: improve landing page layout

--- src/landing/Landing.jsx
+++ src/landing/Landing.jsx
@@ -22,34 +22,65 @@ export default function Landing() {
                         Seja bem-vindo à <strong>Layer 8</strong>
                     </h1>
                     <p>Escolha sua experiência</p>
-                    <hr />
+                    {/* <hr /> */}
                 </div>
             </header>
 
             {/* ================= MAIN ================= */}
             <main>
                 {/* Seção Recicla */}
-                <section className="side white-background" id="recicla">
-                    <button
-                        className="button-to"
-                        id="recicla-btn"
-                        onClick={() => handleId("load")}
-                    >
-                        <Link to="/?p=recicla">Entrar</Link>
-                    </button>
+                <section className="side">
+                    <div className="enter-card" id="recicla">
+                        <button
+                            className="button-to"
+                            id="recicla-btn"
+                            onClick={() => handleId("load")}
+                        >
+                            <Link to="/?p=recicla">Acessar Layer 8 Recicla</Link>
+                        </button>
+                    </div>
                 </section>
 
                 {/* Seção Tecnologia */}
-                <section className="side white-background" id="tecnologia">
-                    <button
-                        className="button-to"
-                        id="tecnologia-btn"
-                        onClick={() => handleId("load")}
-                    >
-                        <Link to="/?p=tecnologia">Entrar</Link>
-                    </button>
+                <section className="side white-background">
+                    <div className="enter-card" id="tecnologia">
+                        <button
+                            className="button-to"
+                            id="tecnologia-btn"
+                            onClick={() => handleId("load")}
+                        >
+                            <Link to="/?p=tecnologia">Acessar Layer 8 Soluções e Serviços</Link>
+                        </button>
+                    </div>
                 </section>
             </main>
+
+            <footer >
+                <div className="feats">
+                    <div className="feat">
+                        <h5>Sustentabilidade</h5>
+                        <p>Compromisso com o meio ambiente</p>
+                    </div>
+                    <div className="line"></div>
+                    <div className="feat">
+                        <h5>Segurança</h5>
+                        <p>Protção do que é essencial.</p>
+                    </div>
+                    <div className="line"></div>
+                    <div className="feat">
+                        <h5>Experiência</h5>
+                        <p>Equipe preparada e pronta para atender você.</p>
+                    </div>
+                    <div className="line"></div>
+                    <div className="feat">
+                        <h5>Inovação</h5>
+                        <p>Tecnologia que gera resultados.</p>
+                    </div>
+                </div>
+                <div className="rights">
+                    <p>&copy; 2026 Layer 8. Todos os direitos reservados.</p>
+                </div>
+            </footer>
         </div>
     );
 }
